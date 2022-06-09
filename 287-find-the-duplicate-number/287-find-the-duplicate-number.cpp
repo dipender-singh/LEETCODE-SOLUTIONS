d10f0f8eebde8472efefe77ad03746dfe09a3db7
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-09 12:16:19 +0530

    Time: 76 ms (99.94%), Space: 61.1 MB (99.30%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,8 +1,11 @@
+//https://www.youtube.com/watch?v=32Ll35mhWg0
+//https://www.youtube.com/watch?v=pavdSf5f5pw (TO UNDERSTAND THE SOLUTION WATCH BOTH VIDEOS)
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
+        /*brute force
         map<int,int> mp;
         int ans=0;
         for(int i = 0 ; i < nums.size() ; i++){
@@ -12,5 +15,20 @@ public:
             }
         }
         return ans;
+        */
+        //two pointer or torotise and hare algo with slow and fast pointer slow moves one fast moves two postion
+        int slow = nums[0];
+        int fast = nums[0];
+        //now we will keep on moving the slow and fast pointer untill and unless they point to the same element
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(fast!=slow){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
     }
 };
