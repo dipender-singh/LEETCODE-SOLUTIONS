5b312795e16492dfbf37f4fd9f661a3f86fa45a5
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-11 22:57:54 +0530

    Time: 11 ms (15.00%), Space: 9.5 MB (78.78%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,49 +1,27 @@
 class Solution {
 public:
-    /*BRUTE FORCE
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        for(int i = 0 ; i < matrix.size() ; i++){ // no of rows
-            for(int j = 0 ; j < matrix[0].size() ; j++){ // no of columns
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
-        }
-        return false;
-        */
-    bool do_binary(vector<vector<int>>& matrix ,int row , int target){
-        vector<int> ans;
-        for(int col = 0 ; col < matrix[0].size() ; col++){ //O(N)
-            ans.push_back(matrix[row][col]); //O(N) EXTRA SPACE
-        }
-        int l = 0;
-        int r = ans.size()-1;
-        while(l<=r){ //O(logN)
-            int mid = (l+r)/2;
-            if(ans[mid]==target){
-                return true;
-            }
-            else if(ans[mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return false;
-    }
-     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        bool ans=false;
-        int n = matrix[0].size();
-        for(int i = 0 ; i < matrix.size(); i++){ //O(N)
-                if(matrix[i][0]<=target and matrix[i][n-1]>=target){
-                     ans = do_binary(matrix,i,target);
-                }
-        }
-        return ans;
-    }
+   
+    //OPTIMAL
+     bool searchMatrix(vector<vector<int>>& matrix, int target){
+         int m = matrix.size();
+         int n = matrix[0].size();
+         int low = 0;
+         int high = m*n-1;
+         bool ans=false;
+         while(low<=high){
+             int mid = low + ((high - low)/2);
+             int r = mid/n; 
+             int c = mid%n;
+             if(matrix[r][c] == target){
+                 return true;
+             }
+             else if(matrix[r][c] > target){
+                 high = mid-1;
+             }
+             else{
+                 low=mid+1;
+             }
+         }
+         return ans;
+     }
 };
