49cfe2029abd2c37b4b403bdc4a9efa718842fcb
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-13 12:54:26 +0530

    Time: 24 ms (67.02%), Space: 19.9 MB (13.73%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //brute
+        ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
         map<int,int>mp;
         int n = nums.size();
         int ans=0;
