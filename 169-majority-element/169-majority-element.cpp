53073592a3a536760e819c24c99b18b2e53fe902
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-13 15:46:37 +0530

    Time: 31 ms (36.84%), Space: 19.7 MB (13.73%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        /*brute but using o(n) extra space and o(n) tc
-        //hashing
+        /*hashing used ans sc is o(n) and tc is o(n) 
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
         map<int,int>mp;
@@ -16,10 +15,26 @@ public:
         }
         return ans;
         */
-        // sorting
+        /* sorting is used space is o(1) and tc is o(nlogn)
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
         sort(nums.begin(),nums.end());
         return nums[nums.size()/2];
+        */
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int count = 0, ele = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(count == 0){
+                ele = nums[i];
+            }
+            if(nums[i]==ele){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return ele;
     }
 };
