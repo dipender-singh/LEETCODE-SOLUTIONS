677cd3db5d55c7a85291564b0c67cd0ca3fb4f1b
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-15 11:10:13 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (25.52%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -23,9 +23,8 @@ public:
         return ans;
     }
     */
-    
-    int memoization(vector<vector<int>> &ans , int i , int j , int m, int n){
-        
+    /*MEMOIZATION:- SAVING THE VALUES IN A MATRIX
+    int memoization(vector<vector<int>> &ans , int i , int j , int m, int n){ 
         if(ans[i][j]!=0){
             return ans[i][j];
         }
@@ -43,9 +42,32 @@ public:
     int uniquePaths(int m, int n){
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);  
-        //DP:- SAVING THE VALUES IN A MATRIX
         vector<vector<int>> ans(m, vector<int> (n,0));
         int a = memoization(ans,0,0,m,n);
+        // last se chal ke mere answer pehle index a gya wohi maine return kara diya
+        // iska matlab mein 2d matrix mein sabse niche se travel karke sabse upar aaya
+        // in this approach my index (i,j) represents the number of unique paths from (i,j) to
+        // (m-1,n-1)
         return a;
         }
+        TC: O(M*N) BECAUSE WE ARE CALCULATING AND STORING THE VALUES IN 2D MATRIX WE ARE DOING 
+          RECURSIVE CALLS BUT WE ARE CHECKING IF A VALUE IS ALREADY THERE FOR THE SPECIFIC VALUES OR 
+          NOT AND THAT VALUE WE ARE LOOKING IN THE 2D MATRIX ITSELF SO IT IS LIKE WE ARE MOVING IN A 
+          2D MATRIX ITSELF
+        SC: O(M*N) BECAUSE WE ARE CALCULATING AND STORING THE VALUES IN 2D MATRIX
+        */
+    //DP APPROACH : ab mai upar se chalte hue niche jaunga or mera answer obviously array ke sabse
+                 // last index pe stored hoga
+    int uniquePaths(int m, int n){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        vector<vector<int>> dp(m,vector<int>(n,1)); //this will make a 2d vector of m*n with all 0
+        dp[0][0]=1;
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 };
