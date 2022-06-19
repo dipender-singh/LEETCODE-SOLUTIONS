e54c415c11edbb08cbea49538831a743534f208c
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-19 16:17:00 +0530

    Time: 922 ms (15.78%), Space: 49.6 MB (48.68%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -3,16 +3,17 @@ public:
     int longestConsecutive(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
+        /*Brute Force
         int final_ans = 1;
         int curr_ans = 1;
         if(nums.size()==0){
             return 0;
         }
         else{
-            sort(nums.begin(),nums.end());
+            sort(nums.begin(),nums.end()); O(nlogn)
             int prev = nums[0];
-            for(int i = 1 ; i < nums.size() ; i++){
-                if(nums[i]==prev){
+            for(int i = 1 ; i < nums.size() ; i++){ O(n)
+                if(nums[i]==prev){ // even if there are repeated elements they can still contribute to                                    // a sequence but they will not be taken into considerattion for                                      // the length of the longest sequence
                     continue;
                 }
                 else if(prev+1 == nums[i]){
@@ -26,28 +27,31 @@ public:
             }
         }
         return final_ans;
-        /*
-        if(nums.size() == 0 ){
-            return 0;
-        }
-        
-        sort(nums.begin(),nums.end());
-        
-        int ans = 1;
-        int prev = nums[0];
-        int cur = 1;
-        
-        for(int i = 1;i < nums.size();i++){
-            if(nums[i] == prev+1){
-                cur++;
-            }
-            else if(nums[i] == prev){
-                cur = 1;
-            }
-            prev = nums[i];
-            ans = max(ans, cur);
-        }
-        return ans;
         */
+        // Optimal Approach can be to use a Hash-Map
+        unordered_set < int > hashSet;
+  for (int num: nums) {
+    hashSet.insert(num);
+  }
+
+  int longestStreak = 0;
+
+  for (int num: nums) {
+    if (!hashSet.count(num - 1)) { // agar humra num hai 1 toh agar humara num-1 i.e. 0 agar woh                                          // present nahi hai toh iska matlab ek sequnce ka humare pass
+                                   // statring point a gya hai toh ab hum uske sare next elements of 
+                                   // the sequence find karenge
+      int currentNum = num;
+      int currentStreak = 1;
+
+      while (hashSet.count(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
     }
 };
