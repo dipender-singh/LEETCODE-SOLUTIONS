abfdda5808524a6df3831ecb4636a65f38e32d64
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-23 15:28:35 +0530

    Time: 8 ms (88.88%), Space: 8.3 MB (74.13%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLongestSubstring(string s) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
-        //optimized
+      /* OPTIMIZED
         if(s.size()==0 || s.size()==1){
             return s.size();
         }
@@ -23,5 +23,21 @@ public:
     maxans = max(maxans, r - l + 1);
   }
   return maxans;
+  */
+       // we will take a freq array that will store the index of a particular character of the string
+        vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1) left = max(mpp[s[right]] + 1, left); // if cond. is true this is exe.
+          
+          mpp[s[right]] = right;// if not then this is exe.
+          
+          len = max(len, right - left + 1);
+          right++;
+      }
+      return len;
     }
 };
