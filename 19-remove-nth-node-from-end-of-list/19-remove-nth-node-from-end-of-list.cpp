b362fb55eab08de3cb0bc097febb2897bcb42c43
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-25 13:56:10 +0530

    Time: 14 ms (8.17%), Space: 10.9 MB (31.83%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,14 +11,14 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        //BRUTE FORCE
+        /*BRUTE FORCE
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
         ListNode* temp = head;
         ListNode* c = head;
         ListNode* p = head;
         int size = 0;
-        while(temp!=NULL){
+        while(temp!=NULL){ //O(N)
             temp=temp->next;
             size++;
         }
@@ -30,13 +30,42 @@ public:
         c->next=NULL;
         return p;
         }
-        while(count!=lim){
+        while(count!=lim){ // O(N-M)
             c=p;
             p=p->next;
             count++;
         }
         c->next=p->next;
         p->next=NULL;
+        return head;
+        TOTAL TC IS:- O(SIZE)+O(SIZE-N) 
+        */
+        //OPTIMIZED : THINK OF IT 
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        // Now we will take a pointer fast ans slow and move fast pointer n steps and a slow pointer 
+        // will be at head for now. So now we have created a gap of 'n' nodes in between the slow and 
+        // fast pointer. So now we will move both slow and fast pointer by one, now when fast pointer 
+        //will reach to the end node, our slow pointer will be at the 'n'th node from the end BECAUSE 
+        //WE HAVE CREATED A GAP OF 'n' NODES BETWEEN SLOW POINTER AND FAST POINTER.
+        ListNode* slow = head;
+        ListNode* fast = head;
+        int x = 1;
+        while(x <= n){
+            fast=fast->next;
+            x++;
+        }
+        if(fast==NULL){ // fast has become null we have reached the end then it means we have to del 
+                        // head
+            head=head->next;
+            slow->next=NULL;
+            return head;
+        }
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next = slow->next->next;
         return head;
     }
 };
