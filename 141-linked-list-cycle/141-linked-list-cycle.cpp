ca231bddd2d3d8fe49dd817aee552e41a6f339f0
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-27 17:26:11 +0530

    Time: 13 ms (68.71%), Space: 8 MB (80.99%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -8,6 +8,7 @@
  */
 class Solution {
 public:
+    /*
      bool hasCycle(ListNode *head) {
          ios_base::sync_with_stdio(0);
          cin.tie(0);cout.tie(0);
@@ -26,7 +27,30 @@ public:
             temp=temp->next;
         }
         return false;
-         //TC: O(N)
-         //SC: O(N)
+         //TC: O(N) for the traversal of the list
+         //SC: O(N) for the Unordered Map
+         //Insertion, Deletion all such tasks in Unordered Map requires O(1) time. 
+    }
+    */
+    bool hasCycle(ListNode *head){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        //OBVIOUSLY WE WILL HAVE TO USE TWO POINTERS HERE
+         if(head==NULL || head->next==NULL){ // In case of Linked List we will first check for 
+                                             // the validity of Head then for the Next of Head. 
+            return false;
+        }
+        ListNode* f = head;
+        ListNode* s = head;
+            s=s->next;
+            f=f->next->next;
+        while(f!=NULL and f->next!=NULL){
+            if(f==s){
+                return true;
+            }
+            s=s->next;
+            f=f->next->next;
+        }
+        return false;
     }
 };
