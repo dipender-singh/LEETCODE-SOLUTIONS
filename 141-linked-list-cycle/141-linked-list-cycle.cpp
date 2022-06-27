dcca880d1dc08af931175b1db1cd703afc5beb78
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-06-27 16:41:26 +0530

    Time: 30 ms (9.75%), Space: 11.1 MB (8.70%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -8,11 +8,14 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+     bool hasCycle(ListNode *head) {
+         ios_base::sync_with_stdio(0);
+         cin.tie(0);cout.tie(0);
         //BRUTE FORCE USING HASHING
         unordered_map<ListNode*, int>mp;
         ListNode* temp = head;
-        if(head==NULL || head->next==NULL){
+        if(head==NULL || head->next==NULL){ // In case of Linked List we will first check for 
+                                            // the validity of Head then for the Next of Head. 
             return false;
         }
         while(temp){
@@ -23,5 +26,7 @@ public:
             temp=temp->next;
         }
         return false;
+         //TC: O(N)
+         //SC: O(N)
     }
 };
