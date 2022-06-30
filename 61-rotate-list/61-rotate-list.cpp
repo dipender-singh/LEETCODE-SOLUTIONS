edede97b99109cf2fc248be1082517857f541653
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-01 01:55:18 +0530

    Time: 10 ms (67.28%), Space: 11.9 MB (6.12%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    
+    /*
     ListNode* reverser(ListNode* temp){
         ListNode* p = NULL;
         ListNode* f = temp;
@@ -72,7 +72,7 @@ public:
         ListNode* temp = head;
         ListNode* res = head;
         int size = 0;
-        while(temp!=NULL){
+        while(temp!=NULL){ //TC IS O(N)
             temp=temp->next;
             size++;
         }
@@ -81,8 +81,46 @@ public:
         }
         k = k % size;
         int x = k;
-        head = reversew(head); // Now i have reversed my whole List
-        head = reversef(head,x);
+        head = reversew(head); // Now i have reversed my whole List TC IS O(N)
+        head = reversef(head,x); // NOW TC IS O(N)
         return head;
+        // TC is O(N+N+N)
+        // SC is O(1)
     }
+    */
+     ListNode* rotateRight(ListNode* head, int k){
+         ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        if(head==NULL){
+            return head;
+        }
+        if(head->next==NULL){
+            return head;
+        }
+        if(k==0){
+            return head;
+        }
+        ListNode* temp = head;
+        ListNode* res  = head;
+        ListNode* nh = NULL;
+        int size = 0;
+        while(temp->next!=NULL){ //TC IS O(N)
+            temp=temp->next;
+            size++;
+        }
+         size++;
+        if(k==size || k%size==0){
+            return head;
+        }
+        k = k % size;
+        int x = size-k-1;
+        while(x!=0){
+            res=res->next;
+            x--;
+        }
+        nh = res->next; 
+        res->next  = NULL; 
+        temp->next = head;
+        return nh; 
+     }
 };
