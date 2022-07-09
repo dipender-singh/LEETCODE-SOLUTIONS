31728d4210043ad287c3aebd1ec26bedddb78344
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-09 18:42:55 +0530

    Time: 20 ms (98.83%), Space: 22.4 MB (55.02%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        //Brute Force can be using Hash-Map or Frequency Array and another can be using XOR
+        // Optimized
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int low = 0;
+        int high = nums.size()-2;
+        while(low<=high){
+            int mid = low + ((high - low)/2);
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                 low=mid+1;   
+                }
+                else{
+                 high = mid-1;
+                }
+            }
+            else{
+                if(nums[mid]==nums[mid+1]){
+                 high = mid-1;
+                }
+                else{
+                 low=mid+1;
+                }
+            }
+        }
+        return nums[low];
+    }
+};
