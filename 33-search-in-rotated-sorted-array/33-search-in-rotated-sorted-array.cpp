2a388edb45db4b07771ed45b327b4f055d9dad9c
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-10 00:41:18 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (29.08%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //Brute Force
+        /*Brute Force
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
         int ans = -1;
@@ -11,5 +11,34 @@ public:
             }
         }
         return ans;
+        */
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int ans = -1;
+        int low = 0;
+        int high = nums.size()-1;
+        while(low<=high){
+            int mid = low + ((high - low)/2);
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[low]<=nums[mid]){ // left part is sorted
+                if(nums[low]<=target and nums[mid]>=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target and nums[high]>=target){
+                    low=mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+    }
+        return -1;
     }
 };
