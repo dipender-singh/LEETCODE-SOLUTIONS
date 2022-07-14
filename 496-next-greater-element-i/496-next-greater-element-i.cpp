a78f3be9aff98b2d6dc471b109fcb3b94b4e4946
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-14 22:45:13 +0530

    Time: 71 ms (5.11%), Space: 8.6 MB (79.93%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -3,43 +3,27 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
-        //Brute Force
-        
-        //since there are no duplicates, we can store them in a map;
-        
-        vector<int> res(nums1.size(), -1); //to be returned, initialize it with -1.
-        stack<int> st;
-        unordered_map<int, int> umap;
-        
-        for(int i=0; i<nums2.size(); i++)
-        {
-            int element = nums2[i];
-            
-            while(!st.empty() && element > st.top())
-            {
-                //NGE of st.top() is element
-				
-                umap[st.top()] = element;
-                st.pop();
+        //BRUTE FORCE
+        vector<int> ans;
+        for(int i = 0 ; i < nums1.size() ; i++){// We will iterate for every element in nums1
+            bool flag = false;
+            for(int j = 0 ; j < nums2.size() ; j++){
+                if(nums1[i]==nums2[j]){
+                    flag = true; // Means we have found the ith element in the nums2 array
+                }
+                if(flag==true and nums2[j]>nums1[i]){
+                    flag=false; // Setting it false means that we have found the element and we have stored it
+                    ans.push_back(nums2[j]);
+                    break;
+                }
             }
-            
-            st.push(element);
-        }
-        
-        for(int i=0; i<nums1.size(); i++)
-        {
-            int ele = nums1[i];
-            
-            if(umap.find(ele) != umap.end())
-            {
-                int nge = umap[ele];
-                res[i] = nge; //push NGE of desired element
+            if(flag==true){
+                ans.push_back(-1); // Flag is still true means that we did find the ith element in nums2 but we 
+                                   // were not able to find it's next greator element in the nums2 array
             }
-                
         }
-        
-        return res;
-        
-        
+        return ans;
+        //TC IS O(M*N)
+        //SC IS O(M) WHEN FOR EVERY ELEMENT OF M WE GET A NEXT GREATOR IN N SIZE ARRAY
     }
 };
