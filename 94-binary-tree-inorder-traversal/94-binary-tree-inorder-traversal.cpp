2fda365288b5575b675a25eead2590d2cbbd0547
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-18 22:03:44 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (15.73%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void inorder(TreeNode* root , vector<int>& ans){
+        if(root==NULL){
+            return ;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        //Brute Force is Recursive and Optimal is Iteratively using Morris Inorder Traversal
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        vector<int> ans; 
+        inorder(root,ans);
+        return ans;
+    }
+};
