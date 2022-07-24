14f6d09a5057ad7b888efa6e11f6fdc6ec7fb129
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-07-24 11:30:55 +0530

    Time: 12 ms (9.96%), Space: 12.2 MB (9.29%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -13,6 +13,12 @@ class Solution {
 public:
 //Using concept of Vertical and Levels as we discussed in the question: Vertical Order Traversal of B-Tree 
 //Now according to me, I can say that we want the last element of every level of every vertical
+
+//Current Solution:-    
+//So what we will do is we will do a level order traversal and will traverse on each level for exactly it's 
+//(size-1) times, doing this when we will reach to the last element of our level which is stored in our queue 
+//and we will exit out of the loop through which we were traversing. Then simply we will store that node's 
+//value in our ans vector and then will store it's left and right child into our queue if they exist 
     vector<int> rightSideView(TreeNode* root) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
@@ -22,9 +28,9 @@ public:
             return {};
         }
         q.push(root);
-        while(q.size()!=0){
-            int n = q.size();
-            for(int i = 0 ; i < n-1 ; i++){
+        while(q.size()!=0){//TC IS O(n)
+            int n = q.size();//If suppose k is the size of each level
+            for(int i = 0 ; i < n-1 ; i++){//TC is O(k-1)
             TreeNode* temp = q.front();
             q.pop();
             if(temp->left!=NULL) q.push(temp->left);
@@ -38,5 +44,7 @@ public:
             if(temp->right!=NULL) q.push(temp->right);
         }
         return ans;
+        //TC IS O(N*K)
+        //SC IS O(N)
     }
 };
