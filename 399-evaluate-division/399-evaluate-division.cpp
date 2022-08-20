6fe9a6913d964c031104c4e8dc05a6b45d91b0cc
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-08-21 01:20:33 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (17.24%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,3 +1,20 @@
+//First try to think that how come this is a graph problem:- I have observed at have assumed that
+        //it is basically forming a directed weighted graph.
+        
+        //Like if our query is like a/e then this eventually means (a/b)*(b/c)*(c/d)*(d/e)
+        //Now if we are seeing each query as a directed weighted edge then we can also visualize the upper 
+        //query as a ---> b ---> c ---> d ---> e. This can give us some insight's of how we can think this 
+        //question as a Graph question and to be more Precise a Directed Weighted Edge Graph is given to 
+        //us.
+        
+        //Since it is a Graph question so first try to think it using BFS/DFS then move to any other Graph 
+        //Algorithm for solving this question.
+
+//Now we don't have a Graph like structure here so to visuliaze this as a graph we will have to
+        //make a graph here.
+        //Can we make an Adjacency List here ?? If not then then what we can use to represent a Graph here
+        //Now we can observe that our Hash-Map will work exactly like our Adjacency List
+
 class Solution {
 public:
 
@@ -15,8 +32,8 @@ public:
             auto it = qe.front(); //Now this 'it' operator will point to a whole pair which was stored at the 
                                   //front of the queue
             
-            if(it.first == dest){
-                return it.second;
+            if(it.first == dest){ //Now this will also cover the case when source and destination nodes are 
+                return it.second; //given same to us from the starting itself then we will simply return 1.
             }
             qe.pop();
             for(auto itt : mp[it.first]){
@@ -49,7 +66,7 @@ public:
             string t = it[1];
             if(mp.find(o)==mp.end() || mp.find(t)==mp.end()){ 
                 //If any one of the node is not present then we can't 
-                //find it's distance, then in that case we will just push -1 in the array.
+                //find value for that query, then in that case we will just push -1 in the array.
                 result.push_back(-1);
                 continue;
             }
@@ -57,4 +74,5 @@ public:
         }
         return result;
     }
+    //Time Complexity:- Since it is BFS so it will be O()
 };
