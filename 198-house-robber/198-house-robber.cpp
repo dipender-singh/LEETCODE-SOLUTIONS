0e7963f60a7a7da9a6fb0aee4a53a40a6bbd3869
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-09-02 00:53:26 +0530

    Time: 3 ms (54.73%), Space: 7.9 MB (6.21%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+
+        int helper(int c, int n, vector<int> &nums, vector<int> &dp)
+        {
+           	//Base Case
+            if (c == n)
+            {   dp[c] = nums[c];
+                return nums[c];
+            }
+            if (c > n)
+            {
+                return 0;
+            }
+            if(dp[c]!=-1){
+            	return dp[c];
+			}
+            /*Now left and right can be used to describe the two scenarios:-
+            1.) We can loot the current the house then leave the next adjacent house and go for the house at                 the third position from the current house.
+            2.) We can leave the current house and can go for the next adjacent house and then decide to loot                 the next adjacent house.*/
+            int left = nums[c] + helper(c + 2, n, nums,dp); //Looted the current house
+            int right = helper(c + 1, n, nums,dp);	//Not Looted the current house
+            dp[c] = max(left, right); //Return the max profit out of both the scenarios
+            return dp[c];
+        }
+
+    int rob(vector<int> &nums)
+    {
+       	//Other Name of this Question is:- Maximum sum of non-adjacent elements
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int ans = 0;
+        int n = nums.size() - 1;
+        vector<int> dp(n+1,-1);
+       	//Now at every house i have two options either i can loot that particular house or i can leave the 
+       	//current house and go towards the next house.
+        ans = helper(0, n, nums,dp);
+        return ans;
+    }
+};
