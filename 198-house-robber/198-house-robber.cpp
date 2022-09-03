99cd9997e342f9b9fe8da3a859d326f15c5607fa
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-09-03 16:10:30 +0530

    Time: 9 ms (7.52%), Space: 7.9 MB (25.56%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -24,6 +24,7 @@ class Solution
             /*int left = nums[c] + helper(c + 2, n, nums, dp);	//Looted the current house
             int right = helper(c + 1, n, nums, dp);	//Not Looted the current house
             dp[c] = max(left, right);	//Return the max profit out of both the scenarios
+// dp[c] represents the maximum profit from '0' to the index 'c'.
             return dp[c];
         }
 
@@ -35,14 +36,14 @@ class Solution
         cout.tie(0);
         int ans = 0;
         int n = nums.size() - 1;
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(nums.size(), -1);
        	//Now at every house i have two options either i can loot that particular house or i can leave the 
        	//current house and go towards the next house.
         ans = helper(0, n, nums, dp);
         return ans;
     }
     */
-    //Dynamic Programming Code:- 
+    /*Dynamic Programming Code:- 
     int rob(vector<int> &nums)
     {
        	//Other Name of this Question is:- Maximum sum of non-adjacent elements
@@ -58,8 +59,38 @@ class Solution
         }
         dp[1] = max(nums[0],nums[1]);
         for(int i = 2 ; i < n ; i++){
-            dp[i] = max((dp[i-2] + nums[i]) , dp[i-1]);
+            dp[i] = max((dp[i-2] + nums[i]) , dp[i-1]); 
+//Each Index of the DP Vector represents the maximum profit we have obatined till now from index 0 to a 
+//particular index without picking no adjacent elements from the sequence of indices [0.....c].
         }
         return dp[n-1];
+    }
+    //Time and Space Complexity:- TC is O(N) and Space is O(N).
+    //Can Further do some Space Optimization and can be done without using the DP Vector
+    */
+    //Memory Optimized Solution
+    int rob(vector<int> &nums)
+    {
+       	//Other Name of this Question is:- Maximum sum of non-adjacent elements
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int prev = nums[0];
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int prev2 = 0;
+        int curr = 0;
+        for(int i = 1 ; i < nums.size() ; i++){
+            int take = nums[i];
+            if(i>1) take = take + prev2;
+            
+            int non_take = prev;
+            
+            curr = max(take,non_take); 
+            prev2 = prev;
+            prev = curr; 
+        }
+        return prev;
     }
 };
