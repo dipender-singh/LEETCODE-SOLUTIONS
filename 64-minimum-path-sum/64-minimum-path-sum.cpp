ecaa01d88dbaf40ff50fc2edeb96c6da0d35ada2
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-09-12 21:55:10 +0530

    Time: 19 ms (34.84%), Space: 10.2 MB (31.22%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -21,7 +21,7 @@ public:
         return ans;
     }
     */
-    
+    /*
     int helper(int i, int j, int m, int n, vector<vector<int>>& dp, vector<vector<int>>& grid){
         if(i>=m || j>=n){
             return -1;
@@ -58,4 +58,29 @@ public:
         return ans;
         //Now the index i,j represents the minimum path sum from m,n to index i,j.
     }
+    */
+    //Dynamic Programming
+    int minPathSum(vector<vector<int>>& grid){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        dp[0][0]=grid[0][0];
+        for(int i = 1 ; i < n ; i++){
+            dp[0][i] = grid[0][i] +dp[0][i-1];
+        }
+        for(int j = 1 ; j < m ; j++){
+            dp[j][0] = grid[j][0] + dp[j-1][0];
+        }
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
+    
+    
 };
