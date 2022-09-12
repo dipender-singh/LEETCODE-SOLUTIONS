93ec89ed02547c5680098784e031c4a35540a931
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-09-12 18:42:59 +0530

    Time: 3 ms (85.13%), Space: 7.9 MB (13.30%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -24,6 +24,7 @@ public:
         return ans;
     }
     */
+    /*
     int helper(int i , int j , int m, int n , vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
         if(i>=m || j>=n){
             return 0;
@@ -51,5 +52,32 @@ public:
         vector<vector<int>> dp(m,vector<int> (n,-1));
         int ans = helper(0,0,m,n,obstacleGrid,dp);
         return ans;
+        //Now here every index in dp[i][j] represents the number of unique ways from (i,j) to (m-1,n-1).
+    }
+    */
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid){
+        //Dynamic Programming
+        int n = obstacleGrid[0].size();
+        int m = obstacleGrid.size();
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        dp[0][0] = 1;
+        if(obstacleGrid[m-1][n-1]==1){
+            return 0;
+        }
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0; j < n ; j++){
+                if(i == 0 and j == 0){
+                    dp[i][j]=1;
+                    }
+                else{
+                    int right = 0;
+                    int down = 0;
+                    if(i>0 and obstacleGrid[i-1][j]!=1) right = dp[i-1][j];
+                    if(j>0 and obstacleGrid[i][j-1]!=1) down = dp[i][j-1];
+                    dp[i][j] = right+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
