39f453a0d6d3bc80a75e74d6c0096c6ffc95ced0
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-09-15 19:29:19 +0530

    Time: 8 ms (71.58%), Space: 9 MB (32.35%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    
-   int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle,int n, 
-vector<vector<int> > &dp) {
-  
-  if(dp[i][j]!=-1)
-  return dp[i][j];
-  
-  if(i==n-1) return triangle[i][j];
-    
-  int down = triangle[i][j]+minimumPathSumUtil(i+1,j,triangle,n,dp);
-  int diagonal = triangle[i][j]+minimumPathSumUtil(i+1,j+1,triangle,n,dp);
-  
-  return dp[i][j] = min(down, diagonal);
-  
-}
-    
-    int minimumTotal(vector<vector<int>>& triangle){
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);cout.tie(0);
-        int ans = 0;
-        int n = triangle.size();
-       vector<vector<int> > dp(n,vector<int>(n,-1));
-    return minimumPathSumUtil(0,0,triangle,n,dp);
+    /*
+    int helper(int i, int j, int m, int n, vector<vector<int>>& triangle){
+        if(i == m-1){
+            return triangle[i][j];
+        }
+        int same = helper(i+1,j,m,n,triangle);
+        int diagonal = helper(i+1,j+1,m,n,triangle);
+        return triangle[i][j] + min(same,diagonal);
     }
     
+    int minimumTotal(vector<vector<int>>& triangle) {
+        //Recursion
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+        int ans = 0;
+        ans = helper(0,0,m,n,triangle);
+        return ans;
+    }
+    */
+    
+    int helper(int i , int j , int m, int n , vector<vector<int>>& triangle , vector<vector<int>>& dp){
+        if(i == m-1){
+            dp[i][j] = triangle[i][j];
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int same = helper(i+1,j,m,n,triangle,dp);
+        int diagonal = helper(i+1,j+1,m,n,triangle,dp);
+        dp[i][j] = triangle[i][j] + min(same,diagonal);
+        return dp[i][j];
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle){
+        //Memoization
+        
+        int m = triangle.size();         //Number of Rows
+        int n = triangle[m-1].size();    //Number of Columns
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        int ans = helper(0,0,m,n,triangle,dp);
+        return ans;
+    }
 };
