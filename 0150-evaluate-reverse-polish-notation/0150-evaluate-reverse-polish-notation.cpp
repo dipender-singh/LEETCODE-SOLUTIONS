dc55ded3d529a0ec5eadbf0cf7a406aa4303d2b9
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2022-12-17 20:20:23 +0530

    Time: 11 ms (89.14%), Space: 12.1 MB (52.63%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<long long> st;
+        stack<long long> st; //As integer will not be able to store the calculation of large integers
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
-        for(auto& it : tokens){
+        for(auto it : tokens){
             if(it=="+"||it=="-"||it=="*"||it=="/"){
-                long long b = st.top(); st.pop();
+                long long b = st.top(); st.pop(); // As stack is long long so, it is self explanatory 
                 long long a = st.top(); st.pop();
                 if(it == "+") a = (a+b);
                 if(it == "-") a = (a-b);
@@ -15,9 +15,11 @@ public:
                 st.push(a);
             }
             else{
-                st.push(stoll(it));
+                st.push(stoll(it));//stol is used with long int and stoll is used with long long int
             }
         }
-        return (int)st.top();
+        return (int)st.top();//Type conversion of float float to int datatype
         }
+    //TC IS O(N) [WHERE N IS THE SIZE OF THE STRING]
+    //SC IS O(N) [WHERE N IS THE SIZE OF THE STRING]
 };
