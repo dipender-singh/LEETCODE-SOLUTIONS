d80f7a7031cdc2632770e9b4c2bcd142d55af089
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-01-09 22:22:56 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (93.88%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    
+    /*
     void helper(TreeNode* root, vector<int>& ans){
         //Base Case
         if(root==NULL){
@@ -21,16 +21,27 @@ public:
         helper(root->left,ans);
         helper(root->right,ans);
     }
+    */
     
     vector<int> preorderTraversal(TreeNode* root) {
         //There are Two ways one is Recursion adn the other way is by using Stack
-        //Recursion
+        //Recursion Time Complexity is O(n) and the Space Complexity is also O(n)[Internal Stack Space is used].
         vector<int> ans;
-        /*if(root==NULL){
-            ans.push_back(root);
-            return ans;
-        }*/
-        helper(root,ans);
+        //Iteratively
+        stack<TreeNode*> st;
+        //Just store the root in the stack and then pop it store it in the vector then store the
+        //Right pehle dalega then Left dalega then again pop karke unki value dali jayegi and then unko
+        if(root==NULL){
+            return {};
+        }
+        st.push(root);
+        while(st.size()!=0){
+            TreeNode* current = st.top();
+            st.pop();
+            ans.push_back(current->val);
+            if(current->right!=NULL) st.push(current->right);
+            if(current->left!=NULL) st.push(current->left);
+        }
         return ans;
     }
 };
