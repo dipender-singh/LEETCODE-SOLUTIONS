2e489ea0068a2c207096bfa787f5f35eceefc419
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-01-26 11:28:28 +0530

    Time: 140 ms (46.13%), Space: 45.3 MB (26.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    /*
     int helper(int i, vector<int>& coins, int amount, vector<vector<int>>& dp){
         if(dp[i][amount]!=-1){
             return dp[i][amount];
@@ -31,9 +31,41 @@ public:
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1,-1));
         int a = helper(n-1,coins,amount,dp);
-        if(a == INT_MAX || a == INT_MAX-1){
+        if(a == INT_MAX || a == INT_MAX-1){ 
+            //Now here INT_MAX-1 will come, when we are at the last index and the 
+            //number of elements given to us is only 1, then this case will get executed.
             return -1;
         }
         return a;
+        //TIme Complexity:- O(N*Target)
+        //Space Complexity:- O(N*Target) + O(Target)[Auxillary Stack Space]
+    }
+    */
+    int coinChange(vector<int>& arr, int T){
+        //Dynamic Programming
+        int n = arr.size();
+       vector<vector<int>> dp(n,vector<int>(T+1,0));
+    
+    for(int i=0; i<=T; i++){
+        if(i%arr[0] == 0)  
+            dp[0][i] = i/arr[0];
+        else dp[0][i] = 1e9;
+    }
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target = 0; target<=T; target++){
+            
+            int notTake = 0 + dp[ind-1][target];
+            int take = 1e9;
+            if(arr[ind]<=target)
+                take = 1 + dp[ind][target - arr[ind]];
+                
+             dp[ind][target] = min(notTake, take);
+        }
+    }
+    
+    int ans = dp[n-1][T];
+    if(ans >=1e9) return -1;
+    return ans;
     }
 };
