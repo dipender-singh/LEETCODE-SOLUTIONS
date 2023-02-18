d205a5f92d443fdfc0113b66b7e192adb723a024
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-02-18 18:50:14 +0530

    Time: 3 ms (62.83%), Space: 10 MB (7.06%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    
+    /*
     TreeNode* helper(TreeNode *root){
         //Base Case
         if(root==NULL){
@@ -29,9 +29,33 @@ public:
     }
     
     TreeNode* invertTree(TreeNode* root) {
-        //Brute Force
+        //Brute Force-> Can also further optimize it and can think of more better approaches also. 
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
         return helper(root);
+    }
+    */
+    //Will try to use BFS
+    TreeNode* invertTree(TreeNode* root){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        if(root==NULL){
+            return NULL;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()!=0){
+            //Now what the BFS does is, it store's all the Nodes of a level in the Queue
+            TreeNode* first = q.front();
+            q.pop();
+            if(first->left!=NULL) q.push(first->left);
+            if(first->right!=NULL) q.push(first->right);
+            
+            //Now just swap the both the nodes
+            TreeNode* temp1 = first->left;
+            first->left = first->right;
+            first->right = temp1;
+        }
+        return root;
     }
 };
