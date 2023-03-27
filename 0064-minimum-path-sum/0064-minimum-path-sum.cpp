a39bbce7e9b5278ebf3cff7b33d1e4b492739f36
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-03-27 22:39:29 +0530

    Time: 11 ms (57.90%), Space: 10.2 MB (36.04%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-     int helper(int i, int j, int m, int n, vector<vector<int>>& dp, vector<vector<int>>& grid){
-        if(i>=m || j>=n){
-            return -1;
-        }
-        if(i==m-1 and j==n-1){
-            dp[i][j]=grid[i][j];
-            return grid[i][j];
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int right = helper(i,j+1,m,n,dp,grid);
-        int down = helper(i+1,j,m,n,dp,grid);
-        if(right == -1){
-        dp[i][j] = grid[i][j] + down;     
-        } 
-        else if(down == -1){
-            dp[i][j] = grid[i][j] + right;
-        }
-        else{
-            dp[i][j] = grid[i][j] + min(right,down);
-        }
-        return dp[i][j];
-    }
-    
-    int minPathSum(vector<vector<int>>& grid){
-        //Memoization
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);cout.tie(0);
+    int minPathSum(vector<vector<int>>& grid) {
+        //Dynamic Programming
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        int ans = helper(0,0,m,n,dp,grid);
-        return ans;
-        //Now the index i,j represents the minimum path sum from m,n to index i,j.
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        //Now we want some initial values in the dp array
+        dp[0][0] = grid[0][0];
+        for(int i = 1 ; i < n ; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i]; 
+        }
+        for(int i = 1 ; i < m ; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = grid[i][j] + min(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[m-1][n-1];
     }
-   
 };
