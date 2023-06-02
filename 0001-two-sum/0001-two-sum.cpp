c6f90566600d54b75776688d727d3b288c14e755
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-06-02 20:26:08 +0530

    Time: 12 ms (80.09%), Space: 10.9 MB (34.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> ans;
-        //Brute Froce
+        //Brute Froce : Time Comlexity is O(N*2) and Space Complexity is O(N)
+        /*
         for(int i = 0 ; i < n-1 ; i++){
             for(int j = i+1 ; j < n ; j++){
                 if(nums[i]+nums[j] == target){
@@ -12,6 +13,23 @@ public:
                     break;
                 }
             }
+        }
+        return ans;
+        */
+        //Better Solution: 
+        //Now why we are using Hash-Map think it like this we are basically mapping two variables to a single target
+        unordered_map<int,int> mp;
+        for(int i = 0 ; i < n ; i++){
+            int curr = target - nums[i];
+            if(mp.empty()){
+                mp[nums[i]] = i;
+            }
+            else if(mp.find(curr)!=mp.end()){
+                ans.push_back(mp[curr]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]] = i;
         }
         return ans;
     }
