c46f180ceb1456d14b28cff10b2f6d31d1852192
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-06-27 15:04:32 +0530

    Time: 4 ms (46.46%), Space: 6.3 MB (60.72%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        //Brute Force: Now here in this Brute Froce solution we are not able to keep a track of the Order of the Elements.
-        //Now we have to keep a track of the order of the elements as well. 
-        
-        //This is also the longest common subsequence kinda variation.
-        
-        //1. Brute Force
-        int j = 0; // For index of str1 (or subsequence
- 
-    // Traverse str2 and str1, and
-    // compare current character
-    // of str2 with first unmatched char
-    // of str1, if matched
-    // then move ahead in str1
-    for (int i = 0; i < t.length() && j < s.length(); i++)
-        if (s[j] == t[i])
-            j++;
- 
-    // If all characters of str1 were found in str2
-    return (j == s.length());
-
+        //Now the Problem says to use the Two-Pointer approach, now what we are goona do is that, we will keep one pointer on the starting of the String "s" and the other Pointer on the starting of the String "t".
+        int i = 0, count = 0;
+        for(int j = 0 ; i < s.size() , j < t.size() ; j++){
+            //Now we will only move ahead when we have encountered an character of String S in the String T.
+            if(s[i] == t[j]){
+                count++;
+                i++;
+            }
+        }
+        if(count == s.size()){
+            return true;
+        }
+        return false;
     }
 };
