61c6c4b394d3069d53656edde4a85c1feb00090d
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-07-08 00:33:32 +0530

    Time: 6 ms (22.88%), Space: 6.9 MB (86.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        //Brute Force:
+    void rotate(vector<vector<int>>& matrix){
+        
+        /*
+        Brute Force: Here we will use an Extra O(n*m) Matrix. 
         int n = matrix.size();
         int m = matrix[0].size();
         vector<vector<int>> ans;
@@ -18,6 +20,36 @@ public:
             for(int j = 0 ; j < m ; j++){
                 matrix[i][j] = ans[i][j];
             }
+        }
+        //Time Complexity : O(n*m) + O(n*m) = O(n*m)
+        //Space Complexity : O(n*m) 
+        */
+        
+        //Better Approach: 
+        //Basically we have to do the Swapping of elements that's it. 
+        
+        //First i will do the Transpose of the Matrix.
+        //Second i will reverse the elements of each row of the Matrix.
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        //First will perform the Transpose of the Matrix.
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < m ; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //Now will reverse the elements of each row.
+        int l = 0, h = m-1;
+        for(int i = 0 ; i < n ; i++){ //This will help me to select each row
+            //Will apply the Two-Pointer Approach to Reverse the Array. 
+            while(l<h){
+                swap(matrix[i][l],matrix[i][h]);
+                l++;
+                h--;
+            }
+            l = 0;
+            h = m-1;
         }
     }
 };
