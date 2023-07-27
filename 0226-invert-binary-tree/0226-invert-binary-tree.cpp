9ca4c3370f2f0f0ac985ab3301e0d550d56cbf9b
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-07-27 12:37:59 +0530

    Time: 4 ms (35.26%), Space: 9.6 MB (52.29%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,51 +11,21 @@
  */
 class Solution {
 public:
-    /*
-    TreeNode* helper(TreeNode *root){
-        //Base Case
-        if(root==NULL){
-            return NULL; 
-        }
-        TreeNode* l = helper(root->left);
-        TreeNode* r = helper(root->right);
-        if(l!=NULL || r!=NULL){
-            TreeNode* temp1 = l;
-            TreeNode* temp2 = r;
-            root->right = temp1;
-            root->left = temp2;
-        }
-        return root;
-    }
-    
     TreeNode* invertTree(TreeNode* root) {
-        //Brute Force-> Can also further optimize it and can think of more better approaches also. 
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);cout.tie(0);
-        return helper(root);
-    }
-    */
-    //Will try to use BFS
-    TreeNode* invertTree(TreeNode* root){
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);cout.tie(0);
-        if(root==NULL){
+       //Brute Force
+        if(root == NULL){
             return NULL;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        while(q.size()!=0){
-            //Now what the BFS does is, it store's all the Nodes of a level in the Queue
-            TreeNode* first = q.front();
-            q.pop();
-            if(first->left!=NULL) q.push(first->left);
-            if(first->right!=NULL) q.push(first->right);
-            
-            //Now just swap the both the nodes
-            TreeNode* temp1 = first->left;
-            first->left = first->right;
-            first->right = temp1;
+        TreeNode* l = NULL;
+        TreeNode* r = NULL;
+        if(root -> left) {
+             l = invertTree(root->left);
         }
+        if(root -> right) {
+             r = invertTree(root->right);
+        }
+        root -> left = r;
+        root -> right = l;
         return root;
     }
 };
