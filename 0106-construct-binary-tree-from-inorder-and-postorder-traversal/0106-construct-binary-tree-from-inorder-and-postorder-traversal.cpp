238e0da274883d0c3559914ccf52b7b732e23852
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-07-29 17:00:03 +0530

    Time: 18 ms (71.37%), Space: 25.8 MB (98.78%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -9,6 +9,14 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+
+Now a very simple and to note thing what is our Preorder Traversal -> Root , Left , Right. Now that means that after the Root Node we have the elements of the Left Sub Tree so we will make the Left Sub Tree First. 
+
+Similarly we have PostOrder Traversal here and it is -> Left , Right , Root. Now when we move from the behind first we encounter the RootNode after that we have the elements of the Right Sub Tree so what we have to do is that we will after creating the RootNode will build the Right Sub Tree first, then we will build the Left Sub Tree.
+
+*/
 class Solution {
 public:
     TreeNode* build(vector<int>& inorder, vector<int>& postorder, int& i, int left, int right) {
@@ -31,5 +39,7 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = postorder.size() - 1;
         return build(inorder, postorder, n, 0, n);
+        //Time Complexity: O(n)
+        //Space Complexity: O(n) 
     }
 };
