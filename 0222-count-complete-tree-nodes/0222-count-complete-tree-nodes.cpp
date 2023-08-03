36b811e2cf6e0ada71244c0b2ef6bf7288beea5e
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-08-04 01:56:47 +0530

    Time: 22 ms (97.42%), Space: 31 MB (12.72%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -9,21 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
-If we have to traverse in the Tree and have to use the Time Complexity to be less than O(n) so can we use Morris Traversal here or not ??
-*/
 class Solution {
 public:
     
-    int helper(TreeNode* root){
+    int height(TreeNode* root){
         if(root == NULL){
             return 0;
         }
-        return 1 + helper(root -> left) + helper(root -> right);
+        return 1 + height(root -> left) + height(root -> right);
+    }
+    
+    int heightr(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+        int ans = 1;
+        while(root -> right!=NULL){
+            ans++;
+            root = root -> right;
+        }
+        return ans;
+    }
+    int heightl(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+        int ans = 1;
+        while(root -> left){
+            ans++;
+            root = root -> left;
+        }
+        return ans;
     }
     
     int countNodes(TreeNode* root) {
-        //Brute Force using Recursion:
-        return helper(root);
+        //How this Concept of Finding the Number of Nodes is related to the Height of the Binary Tree.
+        //What is the Total Number of Nodes if a Binary Tree is a Perfect Binary Tree.
+        
+        //Ok so the Concept here is to check for the Perfect Binary Tree, if the height of the Left Sub Tree is same as that the Height of the Right SubTree then we can say that the Tree is a Perfect Binary Tree and we can use the Direct Formula to Calculate the Number of Nodes in the Perfect Binary Tree, else we can go and calculate the Number of Nodes Recursively.
+        if(root == NULL){
+            return 0;
+        }
+        int l = heightl(root -> left);
+        int r = heightr(root -> right);
+        
+        if( l == r){ //Means that the Tree is a Perfect Binary Tree. 
+            return (pow(2,l+1)-1);
+        }
+        return height(root);
     }
 };
