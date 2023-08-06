dd3169b34108f6fbc134c14983b545714b800f20
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-08-06 19:42:32 +0530

    Time: 3 ms (68.70%), Space: 12 MB (53.57%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,41 +11,32 @@
  */
 class Solution {
 public:
-    
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        //Brute Force
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);cout.tie(0);
-        vector<int> ans;
-        vector<vector<int>> f_ans = {};
-        if(root==NULL){
-            return f_ans;
+        int h = 0;
+//When we are at a Even Level we will have the Values in the Order of Right to Left, and in case of Odd Level Left to Right.
+        if(root == NULL){
+            return {};
         }
+        vector<int> ans;
+        vector<vector<int>> final_ans;
         queue<TreeNode*> q;
-        int curr_level = 0;
         q.push(root);
         while(q.size()!=0){
-            //Now we have to get the number of elements in the current level of the tree and that we can find 
-            //from the size of the queue.
-            int s = q.size();
-            for(int i = 0 ; i < s ; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                ans.push_back(curr->val);
-                if(curr->left!=NULL) q.push(curr->left);
-                if(curr->right!=NULL) q.push(curr->right);
+            int n = q.size();
+                for(int i = 0 ; i < n ; i++){
+                    TreeNode* curr = q.front();
+                    q.pop();
+                    if(curr -> left) q.push(curr -> left);
+                    if(curr -> right) q.push(curr -> right);
+                    ans.push_back(curr -> val);
+                }
+            if(h%2!=0){
+                 reverse(ans.begin(),ans.end());
             }
-            //Now we have to check that previously we were at which level even or odd.
-            if(curr_level%2==0){
-                f_ans.push_back(ans);
-            }
-            else{
-                reverse(ans.begin(),ans.end());
-                f_ans.push_back(ans);
-            }
-            curr_level++;
-            ans={};
+            final_ans.push_back(ans);
+            ans = {};
+            h++;
         }
-        return f_ans;
+        return final_ans;
     }
 };
