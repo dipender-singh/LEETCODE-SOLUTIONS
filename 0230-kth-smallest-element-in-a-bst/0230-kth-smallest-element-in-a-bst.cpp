bb7c475174991c1b7b3a72cc18974ef37fca765b
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-08-07 17:58:17 +0530

    Time: 16 ms (76.59%), Space: 24.1 MB (66.53%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -12,19 +12,38 @@
 class Solution {
 public:
     
-    void helper(TreeNode* root, vector<int>& ans){
+    TreeNode* prev = NULL; 
+    
+    int helper(TreeNode* root, int& k){
         if(root == NULL){
-            return;
+            return -1;
         }
-        helper(root -> left,ans);
-        ans.push_back(root -> val);
-        helper(root -> right,ans);
+        int l = -1;
+        int r = -1;
+        l = helper(root -> left,k);
+        if(k == 0){
+            return prev -> val;
+        }
+        k--;
+        prev = root;
+        r = helper(root -> right,k);
+        if(l != -1){
+            return l;
+        }
+        return r;
     }
     
     int kthSmallest(TreeNode* root, int k) {
-        //Brute Force
-        vector<int> ans;
-        helper(root,ans);
-        return ans[k-1];
+        
+        //This  concept of Subtracting "k" is working just have to check for some steps like what to return and where to subtract the value. 
+        int ans = 0;
+        if(root -> left == NULL and root -> right == NULL and k == 1) {
+            return root->val;
+        }
+        ans = helper(root,k);
+        if(prev!=NULL and ans == -1){
+            return prev->val;
+        }
+        return ans;
     }
 };
