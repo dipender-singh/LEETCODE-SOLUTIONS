3ed092a3f108a4e55b4b9634e1e2b52231ac3e18
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2023-08-11 23:06:00 +0530

    Time: 9 ms (58.44%), Space: 14.2 MB (26.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    
-    void helper(vector<vector<int>>& image, int i, int j, int color, int st){
-        if(i < 0 || j < 0 || i >= image.size() || j >=image[0].size() || image[i][j] != st){
-            return;
-        }
-        if(image[i][j] == st){
-            image[i][j] = color;
-            helper(image,i+1,j,color,st);
-            helper(image,i-1,j,color,st);
-            helper(image,i,j+1,color,st);
-            helper(image,i,j-1,color,st);
-        }
-    }
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        //Brute Force
-        //Using DFS
+        //BFS
+        //Using Queue
+        int n = image.size();
+        int m = image[0].size();
+        queue<pair<int,int>> q;
         int st = image[sr][sc];
-        if(st != color){
-        helper(image,sr,sc,color,st);
+        if(st!=color){
+            q.push({sr,sc});
+            while(q.size()!=0){
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                if(i<0 || j<0 || i >= n || j >= m || image[i][j]!=st){
+                    continue;
+                }
+                if(image[i][j] == st){
+                    image[i][j] = color;
+                    q.push({i+1,j});
+                    q.push({i-1,j});
+                    q.push({i,j+1});
+                    q.push({i,j-1});
+                }
+            }
         }
         return image;
     }
