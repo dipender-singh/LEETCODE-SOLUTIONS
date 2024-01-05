9b7715612c14e9a1f9f214249bb05a30cbb37192
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2024-01-05 23:39:05 +0530

    Time: 15 ms (5.42%), Space: 17.3 MB (5.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        //Now the Problem says to use the Two-Pointer approach, now what we are goona do is that, we will keep one pointer on the starting of the String "s" and the other Pointer on the starting of the String "t".
-        int i = 0, count = 0;
-        for(int j = 0 ; i < s.size() , j < t.size() ; j++){
-            //Now we will only move ahead when we have encountered an character of String S in the String T.
-            if(s[i] == t[j]){
-                count++;
-                i++;
+    bool isSubsequence(string s1, string s2) {
+        //Dynamic Programming
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<string>> dp(n+1,vector<string>(m+1,""));
+
+	for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            if (s1[i] == s2[j]) {
+                dp[i][j] = s1[i] + dp[i + 1][j + 1];
+            } else {
+                dp[i][j] = (dp[i + 1][j].size() >= dp[i][j + 1].size()) ? dp[i + 1][j] : dp[i][j + 1];
             }
         }
-        if(count == s.size()){
-            return true;
+    }
+        string ans = dp[0][0];
+        if(ans.size()!=n) return false;
+        for(int i = 0 ; i < ans.size() ; i++){
+            if(ans[i]!=s1[i]) return false;
         }
-        return false;
+        return true;
     }
 };
